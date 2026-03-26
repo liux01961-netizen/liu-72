34d586baac8677c4c7acaec7a24e3ecdad9eed75
liux01961-netizen <liux01961@gmail.com>
2026-03-26 12:50:52 +0800

    v1

--- mis27/web.py
+++ mis27/web.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 from datetime import datetime
 
 app = Flask(__name__)
@@ -9,11 +9,14 @@ def index():
     link += "<a href=/mis>課程</a><hr>"
     link += "<a href=/today>現在日期時間</a><hr>"
     link += "<a href=/me>關於我</a><hr>"
+    link += "<a href=/welcome?u=香均&d=靜宜資管&c=資訊管理導論>Get傳值</a><hr>"
+    link += "<a href=/account>Post傳值</a><hr>"
+    link += "<a href=/次方根號>次方根號計算</a><hr>"
     return link
 
 @app.route("/mis")
 def course():
-    return "<h1>資訊管理導論</h1>"
+    return "<h1>資訊管理導論</h1><a href=/>返回首頁</a>"
 
 @app.route("/today")
 def today():
@@ -23,6 +26,27 @@ def today():
 @app.route("/me")
 def me():
     return render_template("mis2026b.html")
+
+@app.route("/welcome",methods=["GET"])
+def welcome():
+    user = request.values.get("u")
+    d = request.values.get("d")
+    c = request.values.get("c")
+    return render_template("welcome.html",name = user , dep = d,course = c)
+
+@app.route("/account", methods=["GET", "POST"])
+def account():
+    if request.method == "POST":
+        user = request.form["user"]
+        pwd = request.form["pwd"]
+        result = "您輸入的帳號是：" + user + "; 密碼為：" + pwd 
+        return result
+    else:
+        return render_template("account.html")
+
+@app.route("/次方根號")
+def 次方根號():
+    return render_template("次方根號.ipynb")
 
 if __name__ == "__main__":
     app.run(debug=True)
