8af6c8fc65953ab0a683017c05efb2298db220e7
liux01961-netizen <liux01961@gmail.com>
2026-03-26 12:48:48 +0800

    v1

--- web.py
+++ web.py
@@ -11,6 +11,7 @@ def index():
     link += "<a href=/me>關於我</a><hr>"
     link += "<a href=/welcome?u=香均&d=靜宜資管&c=資訊管理導論>Get傳值</a><hr>"
     link += "<a href=/account>Post傳值</a><hr>"
+    link += "<a href=/次方根號>次方根號計算</a><hr>"
     return link
 
 @app.route("/mis")
@@ -42,6 +43,10 @@ def account():
         return result
     else:
         return render_template("account.html")
+
+@app.route("/次方根號")
+def 次方根號():
+    return render_template("次方根號.ipynb")
 
 if __name__ == "__main__":
     app.run(debug=True)
