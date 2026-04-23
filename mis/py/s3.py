ad090294d362dba9af3da4083ca381801a3f178e
liux01961-netizen <liux01961@gmail.com>
2026-04-23 12:06:17 +0800

    0423

---
+++ mis/py/s3.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://liu-72.vercel.app/me"
+Data = requests.get(url)
+Data.encoding = "utf-8"
+#print(Data.text)
+sp = BeautifulSoup(Data.text, "html.parser")
+result=sp.find("img")
+print(result.get("src"))
